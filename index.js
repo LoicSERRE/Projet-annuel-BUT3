503a7fdb5bae3bfa12c2b54b88f50b5564e5e77f
Loïc SERRE <loic.serre@etu.univ-amu.fr>
2023-12-17 03:33:00 +0100

    Modification du main pour rajouter les configuration d'express

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 import createDatabase from "./Database/initdatabase.js";
+import express from 'express';
+import userPath from "./Path/UsersPath.js";
+import zonePath from "./Path/ZonesPath.js";
+import rolePath from "./Path/RolesPath.js";
+import loginPath from "./Path/LoginPath.js"
+import Jwt from 'jsonwebtoken';
+import UsersServices from "./Services/UsersServices.js";
 
-createDatabase();
+//createDatabase();
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/users', userPath);
+app.use('/zones', zonePath);
+app.use('/roles', rolePath);
+app.use('/login', loginPath);
+app.use('/logout', (req, res) => {
+    console.log('Logout');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
