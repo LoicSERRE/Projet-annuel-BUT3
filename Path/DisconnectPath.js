0cac97873ab820607d7002a7ec3e3dafe60de921
Loïc SERRE <loic.serre@etu.univ-amu.fr>
2023-12-27 01:58:02 +0100

    Ajout de la fonctionalités de deconnexion

--- Path/DisconnectPath.js
+++ Path/DisconnectPath.js
@@ -14,7 +14,18 @@ const disconnectPath = express.Router();
  * @param {object} req - The request
  */
 disconnectPath.post('/', async (req, res) => {
-    console.log('Disconnect');
+    const token = req.headers['authorization'];
+
+    // Check the token
+    Jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ error: 'Invalid token' });
+        }
+
+        await UsersServices.addrevokedtoken(token);
+
+        return res.status(200).json({ message: 'Disconnected' });
+    });
 });
 
 export default disconnectPath;
