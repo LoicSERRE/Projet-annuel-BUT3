645c85ccb445ba31cd70aae594135c4d7a8c6ac3
SERRE Loic <loic.serre@etu.univ-amu.fr>
2024-04-03 00:50:42 +0200

    Ajout de la page de paramètre de compte

--- webapp/src/MainPage/MyAccount.js
+++ webapp/src/MainPage/MyAccount.js
@@ -1,73 +1,140 @@
 import React, { useState } from 'react';
 import SideMenu from './SideMenu.js';
+import Styles from '../Style/Account.module.css';
 
-/**
- * Represent the MyAccount component for the MyAccount page.
- * 
- * @returns {JSX.Element} The rendered MyAccount component.
- */
 function MyAccount() {
-    // Get the access token from the local storage
     const token = localStorage.getItem('token');
-    
-    // Get the user information from the server
-    fetch('http://carto.next-vertices.com:3000/users', {
-        method: 'GET',
-        headers: {
-            Authorization: `${token}`
-        },
-        credentials: 'include'
-    })
+    const id = JSON.parse(atob(token.split('.')[1])).id;
+
+    const [username, setUsername] = useState('');
+    const [oldPassword, setOldPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const [newPasswordConfirmation, setNewPasswordConfirmation] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(JSON.parse(atob(token.split('.')[1])).permissions[0].role_id);
+
+        // Vérifiez si le nouveau mot de passe et sa confirmation correspondent
+        if (newPassword !== newPasswordConfirmation && newPassword !== '') {
+            // Mettre à jour le message d'erreur
+            setErrorMessage('Le nouveau mot de passe ne sont pas identiques');
+            return;
+        } 
+        // Si l'ancien mot de passe est valide
+        else if (CheckPassword(oldPassword) === false) {
+            // Mettre à jour le message d'erreur
+            setErrorMessage('Ancien mot de passe invalide');
+            return;
+        }
+        else {
+            setErrorMessage('');
+        }
+
+        // Envoyer la requête pour mettre à jour le nom d'utilisateur et le mot de passe
+        fetch('http://carto.next-vertices.com:3000/users/' + id, {
+        //fetch('http://localhost:3000/users/' + id, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `${token}`
+            },
+            body: JSON.stringify({
+                username: username,
+                password: newPassword,
+                role_id: id
+            })
+        })
         .then(res => {
             if (!res.ok) {
                 throw new Error(`HTTP error! status: ${res.status}`);
             }
             return res.json();
-        }
-        )
+        })
         .then(data => {
-            console.log(data);
-            setAccountInfo(data);
-        }
-        )
+            console.log('User updated successfully');
+            // Deconnecter l'utilisateur vers la page de connexion
+            window.location.href = '/login';
+            fetch('https://carto.next-vertices.com:3000/logout', {
+            //fetch('http://localhost:3000/logout', {
+                method: 'POST',
+                headers: {
+                    Authorization: `${token}`
+                },
+                credentials: 'include'
+            })
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error(`HTTP error! status: ${res.status}`);
+                }
+                return res.json();
+            })
+            .then(data => {
+                console.log(data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        })
         .catch(err => {
             console.log(err);
-        }
-        );
-
-    const [accountInfo, setAccountInfo] = useState({
-        username: '',
-        email: '',
-        password: ''
-    });
-
-    const handleChange = (e) => {
-        setAccountInfo({
-            ...accountInfo,
-            [e.target.name]: e.target.value
         });
     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    };
+    async function CheckPassword(inputtxt) {
+        // Fonction qui fait une requete à l'api pour générer un token et vériier si le mot de passe est correct
+        try {
+            //const res = await fetch('http://localhost:3000/login', {
+            const res = await fetch('http://carto.next-vertices.com:3000/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: inputtxt
+                })
+            });
+            if (!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+            }
+            return true;
+        } catch (err) {
+            console.log(err);
+            return false;
+        }
+    }
 
     return (
         <>
-            <SideMenu />
-            <div>
-                <h1>Page de Mon Compte</h1>
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        Nom d'utilisateur :
-                        <input type="text" name="username" value={accountInfo.username} onChange={handleChange} />
-                    </label>
-                    <label>
-                        Mot de passe :
-                        <input type="password" name="password" value={accountInfo.password} onChange={handleChange} />
-                    </label>
-                    <input type="submit" value="Mettre à jour" />
-                </form>
+            <div className={Styles.accountBackground}>
+                <SideMenu />
+                <h1 className={Styles.title}>Mon compte</h1>
+                <div className={Styles.content}>
+                    <div className={Styles.textbackground}>
+                        <form onSubmit={handleSubmit} className={Styles.form}>
+                            <label className={Styles.formItem}>
+                                Nom d'utilisateur :
+                                <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                            </label>
+                            <label className={Styles.formItem}>
+                                Ancien mot de passe :
+                                <input type="password" name="oldPassword" value={oldPassword} onChange={(e) => setOldPassword(e.target.value)} />
+                            </label>
+                            <label className={Styles.formItem}>
+                                Nouveau mot de passe :
+                                <input type="password" name="password" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} />
+                            </label>
+                            <label className={Styles.formItem}>
+                                Confirmez le nouveau mot de passe :
+                                <input type="password" name="newPasswordConfirmation" value={newPasswordConfirmation} onChange={(e) => setNewPasswordConfirmation(e.target.value)} />
+                            </label>
+                            {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+                            <input type="submit" value="Mettre à jour" className={Styles.submitButton} />
+                        </form>
+                    </div>
+                </div>
             </div>
         </>
     );
